1125eb06bdb75a46f5f3d66a72518de6aa749648
Josh Demir <s1611045@connect.glos.ac.uk>
2017-03-18 19:35:55 +0000

    Added GUI form

--- cplusplus_calculator_ct4021/CalcForm.h
+++ cplusplus_calculator_ct4021/CalcForm.h
@@ -48,11 +48,17 @@ namespace cplusplus_calculator_ct4021 {
 		/// </summary>
 		void InitializeComponent(void)
 		{
-			this->components = gcnew System::ComponentModel::Container();
-			this->Size = System::Drawing::Size(300,300);
-			this->Text = L"CalcForm";
-			this->Padding = System::Windows::Forms::Padding(0);
+			this->SuspendLayout();
+			// 
+			// CalcForm
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->ClientSize = System::Drawing::Size(284, 261);
+			this->Name = L"CalcForm";
+			this->Text = L"C++ Calculator";
+			this->ResumeLayout(false);
+
 		}
 #pragma endregion
 	};

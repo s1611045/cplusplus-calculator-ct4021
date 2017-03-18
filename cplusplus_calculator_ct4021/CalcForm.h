771027874ead7dc874816e3f0efb0cfd96a27d59
Josh Demir <s1611045@connect.glos.ac.uk>
2017-03-18 19:36:55 +0000

    Resized form

--- cplusplus_calculator_ct4021/CalcForm.h
+++ cplusplus_calculator_ct4021/CalcForm.h
@@ -54,7 +54,7 @@ namespace cplusplus_calculator_ct4021 {
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
-			this->ClientSize = System::Drawing::Size(284, 261);
+			this->ClientSize = System::Drawing::Size(434, 311);
 			this->Name = L"CalcForm";
 			this->Text = L"C++ Calculator";
 			this->ResumeLayout(false);

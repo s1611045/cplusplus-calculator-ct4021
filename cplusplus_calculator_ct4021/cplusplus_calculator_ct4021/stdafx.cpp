ec2e0e03a659397d97329590f38dd3a8ea888826
s1611045 <s1611045@connect.glos.ac.uk>
2017-03-21 11:22:55 +0000

    Removed non-gui files
    Removed non-gui files

--- cplusplus_calculator_ct4021/cplusplus_calculator_ct4021/stdafx.cpp
+++
@@ -1,8 +0,0 @@
-// stdafx.cpp : source file that includes just the standard includes
-// cplusplus_calculator_ct4021.pch will be the pre-compiled header
-// stdafx.obj will contain the pre-compiled type information
-
-#include "stdafx.h"
-
-// TODO: reference any additional headers you need in STDAFX.H
-// and not in this file
